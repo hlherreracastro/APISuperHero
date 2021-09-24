b4cc50143cff0a94aebe6beb65ff8543b9be14f6
Gabo <gabogaruco@gmail.com>
2021-09-23 19:28:14 -0600

    Conexion de botones a vistas stats y familia, listo.

--- APISuperHero/src/app/vistas/biografia/biografia.component.ts
+++ APISuperHero/src/app/vistas/biografia/biografia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-biografia',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BiografiaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    
+  }
+
+  Stats(){
+    this.router.navigate(['nivel-poder']);
+  }
+
+  Family(){
+    this.router.navigate(['conexiones-familia']);
   }
 
 }
